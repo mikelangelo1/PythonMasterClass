808860b42a5f29633662494bb0809217f3d01785
mikelangelo1 <michael.oluwasola@stu.cu.edu.ng>
2024-08-20 23:35:48 +0800

    closures

---
+++ decorators.py
@@ -0,0 +1,14 @@
+# Decorators allows developers to modify the behaviour of a function or a 
+# class. 
+
+
+# Functions are first class objects which means that functions can be used or passed as arguments
+
+
+### Properites of first class functions
+# - You can store the function in a variable.
+# - You can pass the function as a parameter to another function
+# - you can return the function from a function
+# - You can store them in data structures such as hash tables, lists, ...
+
+
